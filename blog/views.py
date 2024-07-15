3ceb2addbc42106cdd71418f7ffff6aaf81a0016
ss <>
2024-07-15 22:00:49 +0900

    クラスベースのオーバーライド

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 
-def index(request): #関数ベース
-    #TOP画面を表示する関数
+
+def index(request):  # 関数ベース
+    # TOP画面を表示する関数
     # return render(request, 'index.html')
-    return render(request, 'blog/index.html')  #変更箇所
+    print("index関数を使ってTOP画面を表示します！関数ベース")
+    return render(request, 'blog/index.html')  # 変更箇所ß
 
 
-class IndexView(TemplateView): #クラスベース
-    #TOP画面を表示するクラス
-    template_name = 'blog/index.html' 
+class IndexView(TemplateView):  # クラスベース
+    # TOP画面を表示するクラス
+    template_name = 'blog/index.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        print("IndexViewを使ってTOP画面を表示します！クラスベース")
+        return self.render_to_response(context)
