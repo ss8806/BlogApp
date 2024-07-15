530bee15c899242b58791fd4fc40d06b73101211
ss <>
2024-07-15 21:04:38 +0900

    クラスベース、関数ベース

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
 
-def index(request):
+def index(request): #関数ベース
     #TOP画面を表示する関数
     # return render(request, 'index.html')
     return render(request, 'blog/index.html')  #変更箇所
+
+
+class IndexView(TemplateView): #クラスベース
+    #TOP画面を表示するクラス
+    template_name = 'blog/index.html' 
