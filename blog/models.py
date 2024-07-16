4a58ef31ae4cc3fbf87b295e40fc6169243bbf82
ss <>
2024-07-17 00:19:15 +0900

    customAdmin

--- blog/models.py
+++ blog/models.py
@@ -6,19 +6,29 @@ from django.db import models
 class Category(models.Model):
     class Meta:
         db_table = "category"
+        verbose_name = "カテゴリ名"  # 追加
+        verbose_name_plural = "カテゴリ名"  # 追加
 
     category_name = models.CharField(
         verbose_name="カテゴリ名", max_length=100, unique=True)
     category_image = models.ImageField(
         verbose_name="カテゴリ画像", upload_to="images/")
 
+    def __str__(self):
+        return self.category_name
+
 
 class Blog(models.Model):
     class Meta:
         db_table = "blog"
+        verbose_name = "ブログ記事"  # 追加
+        verbose_name_plural = "ブログ記事"  # 追加
 
     title = models.CharField(verbose_name="タイトル", max_length=100)
     content = models.TextField(verbose_name="内容")
     postdate = models.DateField(verbose_name="投稿日", auto_now_add=True)
     category = models.ForeignKey(
         Category, on_delete=models.PROTECT, verbose_name="カテゴリ")
+
+    def __str__(self):
+        return self.title
