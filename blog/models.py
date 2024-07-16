41ae58ffa26545528c0d2aa866885a1257de296e
ss <>
2024-07-17 00:00:28 +0900

    migrate

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    class Meta:
+        db_table = "category"
+
+    category_name = models.CharField(
+        verbose_name="カテゴリ名", max_length=100, unique=True)
+    category_image = models.ImageField(
+        verbose_name="カテゴリ画像", upload_to="images/")
+
+
+class Blog(models.Model):
+    class Meta:
+        db_table = "blog"
+
+    title = models.CharField(verbose_name="タイトル", max_length=100)
+    content = models.TextField(verbose_name="内容")
+    postdate = models.DateField(verbose_name="投稿日", auto_now_add=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, verbose_name="カテゴリ")
